eb654738e641e075ad47c45bc7beb6b2fe2ff898
stro0001 <stro0001@stud.ek.dk>
2025-12-09 13:48:00 +0100

    backend

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+async function hentUddannelser() {
+    const skoleNavn = "Copenhagen Business School";
+    const response = await fetch(
+        "http://localhost:8080/api/skole/" + encodeURIComponent(skoleNavn)
+    );
+    const data = await response.json();
 
+    const liste = document.querySelector(".uddannelser-liste");
+
+    data.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item.uddannelsesnavn;
+        liste.appendChild(li);
+    });
+}
+
+hentUddannelser();
